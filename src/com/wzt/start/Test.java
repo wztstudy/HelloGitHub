033d5c16a029420a84e31de665f03e733b3924be
wzt <1547742521@qq.com>
2019-05-14 22:49:22 +0800

    idea git测试5

--- src/com/wzt/start/Test.java
+++ src/com/wzt/start/Test.java
@@ -2,6 +2,6 @@ package com.wzt.start;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("------------");
+        System.out.println("懵逼树上懵逼果");
     }
 }
